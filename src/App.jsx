0180b7818bba8a513194013401335ed3d2784185
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-02-12 16:18:35 +0100

    Added route paths and elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import "./style.css"
 import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import Projects from "./pages/Projects.jsx"
+import Resume from "./pages/Resume.jsx"
+import Contact from "./pages/Contact.jsx"
 
 function App() {
 
@@ -9,10 +13,10 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path={"/"} element={<Home />}/>
-                <Route />
-                <Route />
-                <Route />
-                <Route />
+                <Route path={"/about"} element={<About />}/>
+                <Route path={"/projects"} element={<Projects />}/>
+                <Route path={"/resume"} element={<Resume />}/>
+                <Route path={"/contact"} element={<Contact />}/>
             </Routes>
         </BrowserRouter>
     </>
