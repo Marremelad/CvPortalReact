fd98e3d39304d289e41aa62d5b67fd1297a4d486
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-02-14 12:38:25 +0100

    Added functionality to toggle easter egg

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,25 @@
-﻿import "./footer.css"
+﻿import {useState} from "react";
+import "./footer.css"
 export default function Footer() {
+    const body = document.querySelector("body");
+    const [theBestTeam, setTheBeastTeam] = useState(false)
+
+    const toggleClass = (theBestTeam) => {
+        if (theBestTeam) {
+            body.classList.add("the-best-team-in-the-world")
+        }
+        else {
+            body.classList.remove("the-best-team-in-the-world")
+        }
+    }
+
     return (
         <>
             <footer>
-                <div className="copy-right-symbol">
+                <div className="copy-right-symbol" onClick={() => {
+                    setTheBeastTeam(theBestTeam => !theBestTeam);
+                    toggleClass(theBestTeam)
+                }}>
                     <p>C</p>
                 </div>
 
