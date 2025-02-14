a197cb518a075434dd6b054bc35946c0bd136161
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-02-14 11:11:45 +0100

    Created footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,19 @@
+﻿import "./footer.css"
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <div className="copy-right-symbol">
+                    <p>C</p>
+                </div>
+
+                <p className="copy-right-text">
+                    Created by
+                    <a className="footer-linkedin-link" href="https://www.linkedin.com/in/mauricio-corte-343a6a22a/">
+                        Mauricio Corte
+                    </a>
+                </p>
+            </footer>
+        </>
+    )
+}
