1d99b71d0ec36aff19cfbdd45a226622ae5fb594
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-02-14 11:01:02 +0100

    Removed header tag and navbar component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,6 @@ import Navbar from "../components/navbar/Navbar.jsx"
 export default function About() {
     return (
         <>
-            <header>
-                <Navbar />
-            </header>
-
             <h1>About me!</h1>
         </>
     )
