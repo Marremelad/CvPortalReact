d892a0d32d1e47bd263371ffe24615e733a0f169
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-02-14 12:19:37 +0100

    Renamed component to header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,59 @@
+import {NavLink, Link} from "react-router-dom"
+import {useState} from "react";
+import Shade from "../shade/Shade.jsx";
+import "./navbar.css"
+
+export default function Header() {
+    const [open, setOpen] = useState(false)
+
+    const toggleShade = () => {
+        setOpen(open => !open);
+    }
+
+    return(
+        <>
+            <header>
+                <article
+                    className={"hamburger-menu"}
+                    onClick={() => setOpen(!open)}
+                >
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </article>
+
+                <nav id="sidebar" className={`sidebar ${open ? "open-sidebar" : "closed-sidebar"}`}>
+                    <article className={"close-sidebar-internal"}
+                             onClick={() => setOpen(!open)}
+                    >
+                        &times;
+                    </article>
+
+                    <ul>
+                        <li>
+                            <NavLink to={"/"}>Home</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to={"/about"}>About Me</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to={"/projects"}>Projects</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to={"/resume"}>Resumé</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to={"/contact"}>Contact Me</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Shade openShade={open} onClick={toggleShade}/>
+            </header>
+        </>
+    )
+}
