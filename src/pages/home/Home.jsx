f1058e1406343aa98175e85c36bb9ba98c83c041
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-02-15 12:33:59 +0100

    Removed html code blocks. File now calls modal popup component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,7 @@
 import {useState} from "react";
-import "./home.css"
+import data from "../../assets/json/techStack.json";
+import ModalPopup from "../../components/modalPopups/ModalPopup.jsx";
+import "./home.css";
 
 export default function Home() {
     const [removeTechStackButton, setRemoveTechStackButton] = useState(false)
@@ -24,77 +26,7 @@ export default function Home() {
             </div>
 
             <section id="tech-stack" className={`tech-stack ${removeTechStackButton ? "open-tech-stack" : ""}`}>
-                <article>
-                    <a href="#dot-net-popup">
-                        <p className="dot-net-icon">.NET</p>
-                    </a>
-                </article>
-
-                <article id="dot-net-popup" className="popup">
-                    <a href="#" className="close">&times;</a>
-                    <h2>.NET</h2>
-                    <p>.NET is a Microsoft framework for building applications using C#, VB.NET, and F#. It provides
-                       a large
-                       library for web, desktop, and mobile development, simplifying tasks like database access and
-                       UI
-                       design.
-                    </p>
-                </article>
-                <a href="#" className="close-popup"></a>
-
-                <article>
-                    <a href="#html-popup">
-                        <i className="fa-brands fa-html5 html5-icon"></i>
-                    </a>
-                </article>
-
-                <article id="html-popup" className="popup">
-                    <a href="#" className="close">&times;</a>
-                    <h2>HTML5</h2>
-                    <p>HTML5 is the latest version of the markup language for structuring web content. It adds new
-                       elements
-                       for multimedia, improved form controls, and better support for web applications, making
-                       websites more
-                       interactive and accessible across devices.
-                    </p>
-                </article>
-                <a href="#" className="close-popup"></a>
-
-                <article>
-                    <a href="#css-popup">
-                        <i className="fa-brands fa-css3-alt css3-icon"></i>
-                    </a>
-                </article>
-
-                <article id="css-popup" className="popup">
-                    <a href="#" className="close">&times;</a>
-                    <h2>CSS 3</h2>
-                    <p>CSS 3 is the latest version of the Cascading Style Sheets language used for styling web
-                       pages. It
-                       introduces new features like animations, transitions, and flexible box layouts, allowing for
-                       more
-                       dynamic and responsive designs.
-                    </p>
-                </article>
-                <a href="#" className="close-popup"></a>
-
-                <article>
-                    <a href="#java-script-popup">
-                        <i className="fa-brands fa-js javascript-icon"></i>
-                    </a>
-                </article>
-
-                <article id="java-script-popup" className="popup">
-                    <a href="#" className="close">&times;</a>
-                    <h2>Javascript</h2>
-                    <p>JavaScript is a versatile programming language that enables dynamic behavior on websites. It
-                       allows
-                       for interactive elements, real-time updates, and user-driven actions, making web pages more
-                       lively
-                       and flexible.
-                    </p>
-                </article>
-                <a href="#" className="close-popup"></a>
+                <ModalPopup data={data}/>
             </section>
         </>
     )
