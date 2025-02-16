6fec798dff9423787c1da0d6b164009df9d58933
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-02-16 15:45:59 +0100

    Converted contact page to React

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,35 @@
-import Header from "../../components/header/Header.jsx"
+import "./contact.css"
+
 export default function Contact() {
     return (
         <>
-            <h1>Contact me!</h1>
+            <img className="coat-of-arms" src="../../../public/images/MauricioCoatOfArmsTextTop.png" alt=""/>
+
+            <div className="title-container">
+                <h1 className="contact-title-text">
+                    If you’re interested in collaborating or exchanging ideas, feel free to reach out.
+                </h1>
+            </div>
+
+            <section className="contact-contact-icons">
+                <article>
+                    <a href="https://www.linkedin.com/in/mauricio-corte-343a6a22a/">
+                        <i className="fa-brands fa-linkedin linkedin-icon"></i>
+                    </a>
+                </article>
+
+                <article>
+                    <a href="https://github.com/Marremelad">
+                        <i className="fa-brands fa-github github-icon"></i>
+                    </a>
+                </article>
+
+                <article>
+                    <a href="mailto:corte.mauricio98@gmail.com">
+                        <i className="fa-regular fa-envelope mail-icon"></i>
+                    </a>
+                </article>
+            </section>
         </>
     )
 }
